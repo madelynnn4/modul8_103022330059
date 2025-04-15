29df3df1f95df8ae0dfea6bf142d153f0d8377b5
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-15 08:17:10 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_103022330059
+{
+    public class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public Transfer transfer { get; set; }
+        public string[] methods { get; set; }
+        public Confirmation confirmation { get; set; }
+
+    }
+    public class Transfer
+    {
+        public int treshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+    }
+
+    public class Confirmation
+    {
+        public string en { get; set; }
+        public string id { get; set; }
+    }
+    public class UIConfig
+    {
+        public BankTransferConfig banktransferconfig;
+        public const String filePath = "banktransferconfig.json";
+        public BankTransferConfig ReadConfigFIle()
+        {
+            string configJsonData = File.ReadAllText(filePath);
+            banktransferconfig = JsonSerializer.Deserialize<BankTransferConfig>(configJsonData); return banktransferconfig;
+        }
+        public void WriteConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            string json = JsonSerializer.Serialize(banktransferconfig);
+            File.WriteAllText(filePath, json);
+        }
+        public void SetDefaultConfig()
+        {
+            banktransferconfig = new BankTransferConfig
+            {
+                lang = "en",
+                transfer = new Transfer
+                {
+                    treshold = 25000000,
+                    low_fee = 6500,
+                    high_fee = 15000
+                },
+                methods = new string[] { "RTO (real-time)", "SKN", "RTGS", "BI FAST" },
+                confirmation = new Confirmation
+                {
+                    en = "yes",
+                    id = "ya"
+                }
+            };
+            WriteConfigFile();
+        }
+        public UIConfig()
+        {
+            try
+            {
+                ReadConfigFIle();
+            }
+            catch (Exception)
+            {
+                SetDefaultConfig();
+                WriteConfigFile();
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using modul8_103022330059;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        UIConfig config = new UIConfig();
+        config.ReadConfigFIle();
+
+        if (config.banktransferconfig.lang == "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer:");
+        }
+        else if (config.banktransferconfig.lang == "id")
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di transfer:");
+        }
+
+        if (int.TryParse(Console.ReadLine(), out int amount))
+        {
+            if (amount < config.banktransferconfig.transfer.treshold)
+            {
+                Console.WriteLine($"Transfer fee: {config.banktransferconfig.transfer.low_fee}");
+            }
+            else
+            {
+                Console.WriteLine($"Transfer fee: {config.banktransferconfig.transfer.high_fee}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+
+        Console.WriteLine("Please select a transfer method:");
+        for (int i = 0; i < config.banktransferconfig.methods.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {config.banktransferconfig.methods[i]}");
+        }
+        if (int.TryParse(Console.ReadLine(), out int methodIndex) && methodIndex > 0 && methodIndex <= config.banktransferconfig.methods.Length)
+        {
+            Console.WriteLine($"You selected: {config.banktransferconfig.methods[methodIndex - 1]}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid selection. Please select a valid transfer method.");
+        }
+        Console.WriteLine("Do you want to confirm the transfer? (yes/no)");
+        string confirmation = Console.ReadLine();
+        if (confirmation.ToLower() == "yes" || confirmation.ToLower() == config.banktransferconfig.confirmation.en)
+        {
+            Console.WriteLine("Transfer confirmed.");
+        }
+        else if (confirmation.ToLower() == "no" || confirmation.ToLower() == config.banktransferconfig.confirmation.id)
+        {
+            Console.WriteLine("Transfer cancelled.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid confirmation. Please enter 'yes' or 'no'.");
+        }
+    }    
+}
